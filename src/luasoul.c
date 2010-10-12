11c36accf1c0b25517c39939521eab1c91725bbc
Guillaume Papin <papin_g@epitech.net>
2010-10-12 23:48:16 +0200

    First real commit :) Begin the ncurses window and lua interaction. The scrolling is not interresting for the moment.

--- src/luasoul.c
+++ src/luasoul.c
@@ -5,14 +5,38 @@
 ** Login   <papin_g@epitech.net>
 ** 
 ** Started on  Wed Oct  6 00:43:31 2010 Guillaume Papin
-** Last update Wed Oct  6 00:43:55 2010 Guillaume Papin
+** Last update Tue Oct 12 22:46:40 2010 Guillaume Papin
 */
 
 #include <stdlib.h>
 #include <stdio.h>
+#include "lua_utils.h"
+#include "ui.h"
 
-int	main(void)
+int		main(void)
 {
-  printf("%s\n", "Hello world");
+  lua_State	*L;
+  int		ch;
+
+  /* init lua */
+  L = load_lua();
+  if (L == NULL)
+    return 1;
+
+  /* initialize ncurses */
+  init_ui();
+
+  /* load rc.lua */
+  if (load_config(L) == -1)
+    return 1;
+
+  #include <curses.h>		/* getch() */
+  while ((ch = getch()) != 'q')
+    {
+      call_lua_function(L, "getch", "is", ch, keyname(ch));
+    }
+
+  lua_close(L);
+  ui_close();
   return (0);
 }
