216616a6c7e4bb07d554fddfd48cd73e85937181
Sarcasm <guillaume.papin@epitech.eu>
2010-11-28 20:58:32 +0100

    move url{en,de}code to netsoul directory.

--- src/utils.c
+++ src/utils.c
@@ -24,9 +24,9 @@
 #include <string.h>
 #include "utils.h"
 
-void		error(const char *fmt, ...)
+void            error(const char *fmt, ...)
 {
-  va_list	argp;
+  va_list       argp;
 
   va_start(argp, fmt);
   fprintf(stderr, "error: ");
@@ -37,9 +37,9 @@ void		error(const char *fmt, ...)
 /*
   Duplicate a string.
 */
-char		*luasoul_strdup(const char *s)
+char            *luasoul_strdup(const char *s)
 {
-  char		*res;
+  char          *res;
 
   res = malloc((strlen(s) + 1) * sizeof(*res));
   return res != NULL ? strcpy(res, s) : NULL;
@@ -48,10 +48,10 @@ char		*luasoul_strdup(const char *s)
 /*
   Duplicate at most n characters of the string s.
 */
-char		*luasoul_strndup(const char *s, size_t n)
+char            *luasoul_strndup(const char *s, size_t n)
 {
-  char		*res;
-  size_t	 len = strlen(s);
+  char          *res;
+  size_t         len = strlen(s);
 
   if (n > len)
     n = len;
@@ -62,9 +62,9 @@ char		*luasoul_strndup(const char *s, size_t n)
 /*
   Duplicate a wide-character string.
 */
-wchar_t		*luasoul_wcsdup(const wchar_t *s)
+wchar_t         *luasoul_wcsdup(const wchar_t *s)
 {
-  wchar_t	*res;
+  wchar_t       *res;
 
   res = malloc((wcslen(s) + 1) * sizeof(*res));
   return res != NULL ? wcscpy(res, s) : NULL;
@@ -73,10 +73,10 @@ wchar_t		*luasoul_wcsdup(const wchar_t *s)
 /*
   Duplicate at most n characters of the wide-character string s.
 */
-wchar_t		*luasoul_wcsndup(const wchar_t *s, size_t n)
+wchar_t         *luasoul_wcsndup(const wchar_t *s, size_t n)
 {
-  wchar_t	*res;
-  size_t	len = wcslen(s);
+  wchar_t       *res;
+  size_t        len = wcslen(s);
 
   if (n > len)
     n = len;
@@ -84,10 +84,10 @@ wchar_t		*luasoul_wcsndup(const wchar_t *s, size_t n)
   return res != NULL ? wcsncpy(res, s, n) : NULL;
 }
 
-char		*get_rc(void)
+char            *get_rc(void)
 {
-  char		rc_path[FILENAME_MAX];
-  char		*dir;
+  char          rc_path[FILENAME_MAX];
+  char          *dir;
 
   /* file in current directory ? */
   if (! access(RC_NAME, F_OK))
@@ -105,57 +105,4 @@ char		*get_rc(void)
   /* file not found */
   error("can't find configuration file: `%s'\n", RC_NAME);
   return NULL;
-}
-
-static char		hexa_value(char c)
-{
-  return (c >= '0' && c <= '9') ? c - '0' : (c & 0xDF) - 'A' + 10;
-}
-
-char			*url_decode(char *s)
-{
-  int			i = 0;
-  char			*res = s;
-
-  while (*s) {
-    /* `%' take the 2 following numbers and convert them from hexa to dec */
-    if (*s == '%')
-      {
-	/* case the string end with a `%' */
-	if (! *(++s))
-	  break ;
-	/* first hexadecimal char here */
-	res[i] = hexa_value(*(s++)) * 16;
-	res[i] += hexa_value(*s);
-      }
-    else
-      res[i] = *s;
-    i++;
-    s++;
-  }
-  res[i] = '\0';
-  return res;
-}
-
-char			*url_encode(const char *s)
-{
-  char			*res;
-  int			i = 0;
-
-  res = malloc(3 * sizeof(*res) * strlen(s) + 1);
-  while (*s) {
-    /* reserved characters */
-    if (strchr(" \n\r\v*'();:@&=+$,/?#[]<>~.\"{}|\\-`_^%", *s) != NULL) {
-      res[i++] = '%';
-      res[i++] = "0123456789ABCDEF"[*s/16];
-      res[i] = "0123456789ABCDEF"[*s%16];
-    }
-    /* not reserved */
-    else
-      res[i] = *s;
-    s++;
-    i++;
-  }
-  res[i] = '\0';
-  return res;
 }
