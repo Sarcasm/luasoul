75a93d6cfc2fdd9a93b079d2d1a1f445da5e2e15
Sarcasm <guillaume.papin@epitech.eu>
2010-12-18 22:15:48 +0100

    forgot to initialize sockfd

--- netsoul/create_session.c
+++ netsoul/create_session.c
@@ -1,18 +1,18 @@
 /*
  * create_session.c for luasoul
- * 
+ *
  * Copyright © 2010 Guillaume Papin <guillaume.papin@epitech.eu>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -131,6 +131,8 @@ netsoulSession          *netsoul_create_session(netsoulConfig  *settings,
 
   /* list of user to watch */
   N->watch_list = NULL;
+
+  N->sockfd = -1;
 
   return N;
 }
